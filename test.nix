778c92aa59589b7ccacd19fad08765c57e47ca99
Linus Heckemann <git@sphalerite.org>
2018-12-16 14:12:59 +0100

    Comment test script

--- test.nix
+++ test.nix
@@ -55,9 +55,13 @@
 
   testScript = ''
     startAll;
+    # Wait for all services to finish starting
     $server->waitForUnit("multi-user.target");
+    # Server should start out with no posts in the database and return an empty but successful response at first
     $server->succeed('[ -z "$(curl --fail localhost)" ]');
+    # Making a post should succeed and the server should respond politely
     $server->succeed("curl --fail -F text=testpost localhost | grep Thanks");
+    # And after that, the post should be displayed on the main page.
     $server->succeed("curl --fail localhost | grep testpost");
   '';
 }
